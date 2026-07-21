8f8cdbd1575e5667ddc33a242d7dc00320a1a469
surjakmirko <surjakrmirko@gmail.com>
2026-07-21 18:24:13 -0300

    Implementacion de los endpoints de tipoUsuario

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,13 +1,18 @@
+using Data;
+using Servicios;
 using WebAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ITipoUsuarioServicio, TipoUsuarioServicio>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add Dependency Injection
 
+builder.Services.AddScoped<ITipoUsuarioRepositorio, TipoUsuarioRepositorio>();
+builder.Services.AddScoped<ITipoUsuarioServicio, TipoUsuarioServicio>();
 
 var app = builder.Build();
 
