0af746edfe0495cad41ea280b9f07a7eccb44d2d
surjakmirko <surjakrmirko@gmail.com>
2026-07-21 16:48:29 -0300

    Finalizacion de los DTO de los crud seleccinados

---
+++ DTOs/TipoUsuarioDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace DTOs
+{
+    public class TipoUsuarioDTO
+    {
+        public int Id { get; private set; }
+        public string Descripcion { get; private set; }
+    }
+}
---
+++ DTOs/UsuarioDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace DTOs
+{
+    public class UsuarioDTO
+    {
+        public int Id { get; private set; }
+        public string Email { get; private set; }
+        public string Telefono { get; private set; }
+        public string Password { get; private set; }
+        public string Nombre { get; private set; }
+        public string Apellido { get; private set; }
+        public string Razon_Social { get; private set; }
+        public DateOnly Fecha_Nacimiento { get; private set; }
+        public int TipoUsuarioId { get; private set; }
+
+        public string? TipoUsuarioNombre { get; private set; }
+
+    }
+}
