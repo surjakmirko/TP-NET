ae76c2f055c10ceb185dd9d0a769926bfc0ed78c
Lautaro_Lagostina <laulagostina@gmail.com>
2026-08-27 11:15:14 -0300

    Prueba de Formulario Login t TipoCancha

---
+++ Data/TipoCanchaRepositorio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Modelo.Dominio;
+
+namespace Data
+{
+    public class TipoCanchaRepositorio : ITipoCanchaRepositorio
+    {
+        private readonly DbContext _context;
+        public TipoCanchaRepositorio(DbContext context)
+        {
+            _context = context;
+        }   
+        public async Task<TipoCancha?> GetAsync(int id)
+        {
+            return await _context.Set<TipoCancha>().FindAsync(id);
+        }
+        public async Task<IEnumerable<TipoCancha>> GetAllAsync()
+        {
+            return await _context.Set<TipoCancha>().ToListAsync();
+        }
+    }
+}
---
+++ WindowsForms/LoginForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsForms
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(75, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Usuario";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(75, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(134, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(75, 135);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(134, 23);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(75, 117);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Contraseña";
+            label2.Click += this.label2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(112, 189);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Iniciar Sesión";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // LoginForm
+            // 
+            ClientSize = new Size(284, 261);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "LoginForm";
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button1;
+    }
+}
